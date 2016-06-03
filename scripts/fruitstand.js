9724399272b270be13820c493cae6205d8ae77ae
anna baker <annakatherinebaker@gmail.com>
2016-06-03 11:07:18 -0500

    added base price

--- scripts/fruitstand.js
+++ scripts/fruitstand.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   console.log('hello from jquery');
 });
 
-var currentPrice = [0,0,0,0];
+var currentPrice = [5.0, 5.0, 5.0, 5.0];
 var fruit = [];
 
 
@@ -35,11 +35,19 @@ console.log(randomNumberMaker(-0.50, 0.50));
 var timer = setInterval(function(){calcPrice(); }, 1000); //remember to change this to 15000
 var calcPrice = function(){
  for (var i = 0; i < currentPrice.length; i++) {
-    currentPrice[i] = Number(randomNumberMaker(-0.50, 0.50));
+    currentPrice[i] = currentPrice[i] += Number(randomNumberMaker(-0.50, 0.50));
+    if (currentPrice[i] > 9.99){
+      currentPrice[i] = 9.99;
+    } else if (currentPrice[i] < 0.50) {
+        currentPrice[i] = 0.50;
+    } else {
+
+    }
+
 }
-$('#applePrice').html(currentPrice[0]);
-$('#bananaPrice').html(currentPrice[1]);
-$('#grapePrice').html(currentPrice[2]);
-$('#orangePrice').html(currentPrice[3]);
+$('#applePrice').html(currentPrice[0].toFixed(2));
+$('#bananaPrice').html(currentPrice[1].toFixed(2));
+$('#grapePrice').html(currentPrice[2].toFixed(2));
+$('#orangePrice').html(currentPrice[3].toFixed(2));
 //console.log(currentPrice);
  };
